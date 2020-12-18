3233b4f381410d05c7ea0809ac721129cc55e333
Mark Edward Ratio <markedwardratio@gmail.com>
2020-12-18 12:54:06 +0800

    Add code for displaying messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var formatDistance = require('date-fns/formatDistance');
+
+// Sample messages
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', {
+		title: 'Mini Message Board',
+		messages,
+		formatDistance,
+	});
 });
 
 module.exports = router;
